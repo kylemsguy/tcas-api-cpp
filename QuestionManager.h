fca151ff2d17d01da54396bb897b8a90d2bf4cf7
Kyle Zhou <kylezhou2002@hotmail.com>
2016-01-06 23:51:13 -0800

    more work on the backend

--- QuestionManager.h
+++ QuestionManager.h
@@ -3,11 +3,18 @@
 
 #include <vector>
 
+#include "Question.h"
+#include "Answer.h"
+
 namespace TCaS_Backend {
 	class QuestionManager {
 		std::vector<Question> questionAns;
 	public:
-
+		void askQuestion(std::string question);
+		std::vector<Question> getQuestions();
+		void deleteQuestion(Question question);
+		void deleteAnswer(Answer answer);
+		
 	};
 }
 
---
+++ TCaSRequest.h
@@ -0,0 +1,12 @@
+#ifndef TCAS_REQUEST_H
+#define TCAS_REQUEST_H
+
+namespace TCaS_Backend {
+	class Request {
+	private:
+		
+	public:
+	}
+}
+
+#endif
